73c1204e2b7dd54d1a6e3704bc7ae41c8c5a4105
Dmitry Sviridov <sviridov.dvv@gmail.com>
2022-02-12 23:50:41 +0300

    Fix old hash router and remove old workflow file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import store from 'state/store';
 import App from 'components/AppContainer';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
