15c75da873f372a09851592e24be4575a6730a03
Dmitrii Sviridov <sviridov.dvv@gmail.com>
2023-07-30 23:44:59 +0400

    Update API URL and add .nvmrc file

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://vc-api.dmitrysviridov.com/api/v1/auth/'
+  baseURL: 'https://vc.dmitriisviridov.com/api/v1/',
 });
 
 const authAPI = {
   async login(username, password) {
-    return axiosInstance.post('login', { username, password });
+    return axiosInstance.post('auth/login', { username, password });
   },
   async register(username, password) {
-    return axiosInstance.post('register', { username, password });
-  }
+    return axiosInstance.post('auth/register', { username, password });
+  },
 };
 
 export default authAPI;
